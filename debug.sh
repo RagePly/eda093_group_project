48771d3949b6f3af3b1598282c98ba3cf0cb188f
Hugo Lom <RagePly@users.noreply.github.com>
2026-09-14 10:33:16 +0200

    Update helper scripts

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
-
-docker run --rm -it -v ./lab_1:/work eda093:latest gdb --args ./build/lsh
+#!/bin/sh
+exec docker run --rm -it -v ./lab_1:/work -v ./rptree/:/work/rptree eda093:latest gdb --args ./build/lsh
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it -v ./lab_1:/work eda093:latest ./build/lsh
+exec docker run --rm -it -v ./lab_1:/work eda093:latest ./build/lsh
