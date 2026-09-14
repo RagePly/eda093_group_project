23b756cf165ccba83cb00c2a1af061ff9c865d62
Hugo Lom <RagePly@users.noreply.github.com>
2026-09-14 10:39:42 +0200

    Enable strace support

--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec docker run --rm -it -v ./lab_1:/work -v ./rptree:/work/rptree eda093:latest bash
+exec docker run --rm -it --cap-add=SYS_PTRACE -v ./lab_1:/work -v ./rptree:/work/rptree eda093:latest tmux
